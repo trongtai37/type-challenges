d49d48d28c30aabe2867b324d8a3271422f42346
Tai Nguyen Trong <trongtaind37@gmail.com>
2022-09-30 18:10:01 +0700

    feat: medium challenges

---
+++ medium/1367_remove_index_signature.ts
@@ -0,0 +1,82 @@
+/*
+  1367 - Remove Index Signature
+  -------
+  by hiroya iizuka (@hiroyaiizuka) #medium 
+  
+  ### Question
+  
+  Implement `RemoveIndexSignature<T>` , exclude the index signature from object types.
+  
+  For example:
+  
+  ```
+  
+  type Foo = {
+    [key: string]: any;
+    foo(): void;
+  }
+  
+  type A = RemoveIndexSignature<Foo>  // expected { foo(): void }
+  
+  ```
+  
+  > View on GitHub: https://tsch.js.org/1367
+*/
+
+/* _____________ Your Code Here _____________ */
+type IsDynamicKey<K> = string extends K
+  ? true
+  : number extends K
+  ? true
+  : bigint extends K
+  ? true
+  : symbol extends K
+  ? true
+  : false;
+
+type RemoveIndexSignature<T> = {
+  [key in keyof T as Exclude<PropertyKey, key> extends PropertyKey
+    ? key
+    : never]: T[key];
+};
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type Foo = {
+  [key: string]: any;
+  foo(): void;
+};
+
+type Bar = {
+  [key: number]: any;
+  bar(): void;
+  0: string;
+};
+
+const foobar = Symbol('foobar');
+type FooBar = {
+  [key: symbol]: any;
+  [foobar](): void;
+};
+
+type Baz = {
+  bar(): void;
+  baz: string;
+};
+
+type X = RemoveIndexSignature<Foo>;
+
+type cases = [
+  Expect<Equal<RemoveIndexSignature<Foo>, { foo(): void }>>,
+  Expect<Equal<RemoveIndexSignature<Bar>, { bar(): void; 0: string }>>,
+  Expect<Equal<RemoveIndexSignature<FooBar>, { [foobar](): void }>>,
+  Expect<Equal<RemoveIndexSignature<Baz>, { bar(): void; baz: string }>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/1367/answer
+  > View solutions: https://tsch.js.org/1367/solutions
+  > More Challenges: https://tsch.js.org
+*/
---
+++ medium/949_anyof.ts
@@ -0,0 +1,57 @@
+/*
+  949 - AnyOf
+  -------
+  by null (@kynefuk) #medium #array
+  
+  ### Question
+  
+  Implement Python liked `any` function in the type system. A type takes the Array and returns `true` if any element of the Array is true. If the Array is empty, return `false`.
+  
+  For example:
+  
+  ```ts
+  type Sample1 = AnyOf<[1, '', false, [], {}]> // expected to be true.
+  type Sample2 = AnyOf<[0, '', false, [], {}]> // expected to be false.
+  ```
+  
+  > View on GitHub: https://tsch.js.org/949
+*/
+
+/* _____________ Your Code Here _____________ */
+type FalsyVal = [] | 0 | '' | false;
+type IsEmptyInterface<T> = [keyof T] extends [never] ? true : false;
+
+type Falsy<T> = T extends FalsyVal ? true : IsEmptyInterface<T>;
+
+type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
+  ? Falsy<F> extends true
+    ? AnyOf<R>
+    : true
+  : false;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<
+    Equal<AnyOf<[1, 'test', true, [1], { name: 'test' }, { 1: 'test' }]>, true>
+  >,
+  Expect<Equal<AnyOf<[1, '', false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, 'test', false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, '', true, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [1], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], { 1: 'test' }]>, true>>,
+  Expect<
+    Equal<AnyOf<[0, '', false, [], { name: 'test' }, { 1: 'test' }]>, true>
+  >,
+  Expect<Equal<AnyOf<[0, '', false, [], {}]>, false>>,
+  Expect<Equal<AnyOf<[]>, false>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/949/answer
+  > View solutions: https://tsch.js.org/949/solutions
+  > More Challenges: https://tsch.js.org
+*/
